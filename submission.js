f6be28adc4170158803ab43f27c079639f7cf322
zhulkhoirrifat <zhulkhoirrifat@gmail.com>
2025-08-07 07:57:52 +0800

    add submission 5 - while

--- submission.js
+++ submission.js
@@ -150,3 +150,31 @@ console.log("\n")
 for (let i = 20; i >= 2; i-=2) {
     console.log(i, "- I will become a developer"); 
 }  
+
+// =======================
+// Submission 5
+// =======================
+
+i = 1
+while (i <= 10) {
+    console.log("Nomor urut ke-" + i);
+    i++;
+}
+
+i = 10
+while (i >= 1) {
+    console.log("Hitung mundur:", i);
+    i--;
+}
+
+i = 2
+while (i <= 20) {
+    console.log(i, "- I love coding"); 
+    i += 2;
+}
+
+i = 20
+while (i >= 2) {
+    console.log(i, "- I will become a developer"); 
+    i -= 2;
+}
