f3a4979862ac8d501800879f025d9936d13c8a95
zhulkhoirrifat <zhulkhoirrifat@gmail.com>
2025-08-15 14:09:14 +0800

    add: submission no3

--- tugas-mysql/db_review_movie_backup.sql
+++ tugas-mysql/db_review_movie_backup.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+g-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: localhost    Database: db_review_movie
 -- ------------------------------------------------------
